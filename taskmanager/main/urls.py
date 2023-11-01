6ed8cd1a2bf2345f49d6cfbd0ddeccfc0a6bd408
Edem Sabitov <edem.sabitov@gmail.com>
2023-11-01 10:52:12 +0100

    Add files via upload

---
+++ taskmanager/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('contacts', views.contacts, name='contacts'),
+    path('create', views.create, name='create'),
+    path('delete', views.delete, name='delete')
+]
