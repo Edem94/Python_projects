477b9229ad20e0c114ea1c24322e440317bee563
Edem Sabitov <edem.sabitov@gmail.com>
2023-12-05 19:41:38 +0100

    Add files via upload

---
+++ django-application-website/mysite/shopapp/views.py
@@ -0,0 +1,127 @@
+from json import dumps
+
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, reverse, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from .models import Product, Order
+
+
+class ShopIndexView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {
+        }
+        return render(request, 'shopapp/shop-index.html', context=context)
+
+
+class ProductDetailsView(DetailView):
+    template_name = "shopapp/products-details.html"
+    model = Product
+    context_object_name = "product"
+
+
+class ProductsListView(ListView):
+    template_name = "shopapp/products-list.html"
+    context_object_name = "products"
+    queryset = Product.objects.filter(archived=False)
+
+
+class ProductCreateView(UserPassesTestMixin, CreateView):
+    def test_func(self):
+        return self.request.user.is_authenticated
+
+    model = Product
+    fields = "name", "price", "description", "discount"
+    success_url = reverse_lazy("shopapp:products_list")
+
+    def form_valid(self, form):
+        new_response = super().form_valid(form)
+        form.instance.created_by = self.request.user
+        form.save()
+        return new_response
+
+
+class ProductUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Product
+    fields = "name", "price", "description", "discount", 'created_by'
+    template_name_suffix = "_update_form"
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_authenticated
+
+    def get_queryset(self, *args, **kwargs):
+        if not self.request.user.is_superuser:
+            return super().get_queryset(*args, **kwargs).filter(
+                created_by=self.request.user
+            )
+        return super().get_queryset(*args, **kwargs)
+
+    def get_success_url(self):
+        return reverse(
+            "shopapp:product_details",
+            kwargs={"pk": self.object.pk},
+        )
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("shopapp:products_list")
+
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        self.object.archived = True
+        self.object.save()
+        return HttpResponseRedirect(success_url)
+
+
+class OrdersListView(LoginRequiredMixin, ListView):
+    queryset = (
+        Order.objects
+        .select_related("user")
+        .prefetch_related("products")
+    )
+
+
+class OrderDetailView(PermissionRequiredMixin, DetailView):
+    permission_required = 'shopapp.view_order'
+    queryset = (
+        Order.objects
+        .select_related("user")
+        .prefetch_related("products")
+    )
+
+
+class ProductsDataExportView(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        products = Product.objects.order_by('pk').all()
+        products_data = [
+            {
+                'pk': product.pk,
+                'name': product.name,
+                'price': product.price,
+                'archived': product.archived
+            }
+            for product in products
+        ]
+        return JsonResponse({'products': products_data})
+
+
+class OrdersDataExportView(UserPassesTestMixin, View):
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def get(self, request: HttpRequest) -> JsonResponse:
+        orders = Order.objects.order_by('pk').all()
+        orders_data = [
+            {
+                'pk': order.pk,
+                'address': order.delivery_address,
+                'promocode': order.promocode,
+                'user': order.user.pk,
+                'products': list(order.products.all().values()),
+            }
+            for order in orders
+        ]
+        return JsonResponse({'orders': orders_data})
